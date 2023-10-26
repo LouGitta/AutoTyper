ef3ad0e51adf0b73f3da201af56ec1293d168c48
LouGitta <lougitta@gmail.com>
2023-10-26 18:52:46 +0200

    Input name

--- Autotyper.py
+++ Autotyper.py
@@ -2,9 +2,14 @@ import time
 import keyboard
 
 while True:
-    duration = int(input("combien de temps en minutes? :"))
+    duration = int(input("How many time should i wait (minutes) ? : "))
     while duration >= 0:
         duration -= 1
         keyboard.press_and_release("a")
         keyboard.press_and_release("enter")
         time.sleep(60)
+
+
+
+
+
